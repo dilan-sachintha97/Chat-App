7897fb71ba10949a36f1dbf7e9423f97ae320c6d
dilan-sachintha97 <dil97an@gmail.com>
2022-12-04 10:37:47 +0530

    2

--- src/com/fastcloud/chat_app/controller/ServerFormController.java
+++ src/com/fastcloud/chat_app/controller/ServerFormController.java
@@ -1,4 +1,11 @@
 package com.fastcloud.chat_app.controller;
 
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ServerFormController {
+    public TextField txtMessageBox;
+
+    public void shutdownServerOnClicked(MouseEvent mouseEvent) {
+    }
 }
